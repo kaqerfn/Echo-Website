47185908c5c340efb8a30f6bf7ee37a58a7c0633
kaqerfn <kaqerfn@gmail.com>
2026-08-06 19:43:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,19 @@
 document.querySelectorAll('a[href^="#"]').forEach(link => {
 
-    link.addEventListener("click", function(e) {
+    link.addEventListener("click", function(e){
 
-        const section = document.querySelector(
+        const target = document.querySelector(
             this.getAttribute("href")
         );
 
-        if (section) {
+        if(target){
             e.preventDefault();
 
-            section.scrollIntoView({
-                behavior: "smooth"
+            target.scrollIntoView({
+                behavior:"smooth"
             });
         }
 
     });
 
-});
-
-
-// Simple reveal animation
-
-const elements = document.querySelectorAll(
-    ".card, .package, .hero-content, .heading"
-);
-
-const observer = new IntersectionObserver(entries => {
-
-    entries.forEach(entry => {
-
-        if(entry.isIntersecting){
-            entry.target.classList.add("show");
-        }
-
-    });
-
-}, {
-    threshold:0.15
-});
-
-
-elements.forEach(el => {
-    el.classList.add("hidden");
-    observer.observe(el);
 });
