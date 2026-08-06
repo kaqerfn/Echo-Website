b2436fc56d81a490c509f8c06d11c5ca07045a6c
kaqerfn <kaqerfn@gmail.com>
2026-08-06 19:25:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Orbit Loaded");
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+
+        const target = document.querySelector(link.getAttribute("href"));
+
+        if(target){
+            target.scrollIntoView({
+                behavior:"smooth"
+            });
+        }
+    });
+});
